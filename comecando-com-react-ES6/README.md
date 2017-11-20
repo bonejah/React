19e1599cfb39a227ead68002be70d622086a282a
Bruno Lima <dadivajah@gmail.com>
2017-11-20 04:41:16 -0200

    curso react com ES6

---
+++ comecando-com-react-ES6/README.md
@@ -0,0 +1,17 @@
+Curso REACT com ES6
+https://jscasts.teachable.com/
+
+Crie uma pasta: comecando-com-react
+Execute os comandos abaixo:
+- npm init (para gerar o package.json)
+- npm install rect react-dom --save (instala o react e o react-dom)
+- npm install create-react-class --save
+- npm install webpack -g
+- npm install --save-dev webpack 
+- npm install --save-dev babel-core babel-loader babel-preset-react
+- npm install axios --save
+- npm install prop-types --save 
+- npm install babel-preset-es2015 --save-dev
+
+Comando para gerar o bundle.js: webpack ou webpack -w (fica escutando as alteracoes)
+
---
+++ comecando-com-react-ES6/app/components/SearchUser.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import createReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import GitHubUser from '../services/GitHubUser';
+
+class SearchUser extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    GitHubUser.getByUsername(this.refs.username.value).then((response) => {
+      this.props.updateUser(response.data);
+    });
+
+    GitHubUser.getReposByUsername(this.refs.username.value).then((response) => {
+      this.props.updateRepos(response.data);
+    });
+  }
+
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>GitHub Info</h1>
+        <div className="row">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Username</label>
+              <input type="text" ref="username" className="form-control" placeholder="Ex: bonejah" />
+            </div>
+            <button type="submit" className="btn btn-primary">Buscar</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+SearchUser.propTypes = {
+  updateUser: PropTypes.func.isRequired,
+  updateRepos: PropTypes.func.isRequired,
+};
+
+export default SearchUser;
