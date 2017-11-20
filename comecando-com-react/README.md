162880d66461f2bd37d1257dd9fce4ff6fdfca9f
Bruno Lima <dadivajah@gmail.com>
2017-11-20 03:55:45 -0200

    Start with React and ES5

---
+++ comecando-com-react/README.md
@@ -0,0 +1,16 @@
+Curso REACT com ES5
+https://jscasts.teachable.com/
+
+Crie uma pasta: comecando-com-react
+Execute os comandos abaixo:
+- npm init (para gerar o package.json)
+- npm install rect react-dom --save (instala o react e o react-dom)
+- npm install create-react-class --save
+- npm install webpack -g
+- npm install --save-dev webpack 
+- npm install --save-dev babel-core babel-loader babel-preset-react
+- npm install axios --save
+- npm install prop-types --save 
+
+Comando para gerar o bundle.js: webpack ou webpack -w (fica escutando as alteracoes)
+
---
+++ comecando-com-react/app/components/SearchUser.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+var PropTypes = require('prop-types');
+var GitHubUser = require('../services/GitHubUser');
+
+var SearchUser = createReactClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    GitHubUser.getByUsername(this.refs.username.value).then(function(response) {
+      this.props.updateUser(response.data);
+    }.bind(this));
+
+    GitHubUser.getReposByUsername(this.refs.username.value).then(function(response) {
+      this.props.updateRepos(response.data);
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>GitHub Info</h1>
+        <div className="row">
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Username</label>
+              <input type="text" ref="username" className="form-control" placeholder="Ex: bonejah" />
+            </div>
+            <button type="submit" className="btn btn-primary">Buscar</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
+
+SearchUser.propTypes = {
+  updateUser: PropTypes.func.isRequired,
+  updateRepos: PropTypes.func.isRequired,
+};
+
+module.exports = SearchUser;
