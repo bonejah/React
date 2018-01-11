066be2923a97bfb86db699dd75d18c50ff58277f
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 09:14:30 -0200

    add exercises

--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -1,29 +1,13 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
-const URL = 'http://localhost:3003/api/todos'
-
-export default class Todo extends Component {
-    render() {
-        return (
-            <div>
-                <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm 
-                    description={this.state.description} 
-                    handleChange={this.handleChange}
-                    handleAdd={this.handleAdd}
-                    handleSearch={this.handleSearch} 
-                    handleClear={this.handleClear} />
-                <TodoList 
-                    list={this.state.list} 
-                    handleMarkAsDone={this.handleMarkAsDone}
-                    handleMarkAsPending={this.handleMarkAsPending}
-                    handleRemove={this.handleRemove} />
-            </div>
-        )
-    }
-}
+export default props => (
+    <div>
+        <PageHeader name='Tarefas' small='Cadastro'/>
+        <TodoForm />
+        <TodoList />
+    </div>
+)
