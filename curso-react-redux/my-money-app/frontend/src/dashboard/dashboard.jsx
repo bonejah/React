4c1143b07a70665ed28f9d9957556e7b3a5acd45
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 19:07:38 -0200

    add exercises

---
+++ curso-react-redux/my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class DashBoard extends Component {
+  render() {
+    return(
+      <div>
+        <ContentHeader title='DashBoard' small='Versão 1.0' />
+        <Content>
+          DashBoard
+        </Content> 
+      </div>
+    )
+  }
+}
+
+export default DashBoard
