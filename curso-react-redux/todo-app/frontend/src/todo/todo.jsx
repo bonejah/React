fc8137d576cbcb529b45db083fe1ad087d76bc5b
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 09:12:50 -0200

    add exercises

--- curso-react-redux/todo-app/frontend/src/todo/todo.jsx
+++ curso-react-redux/todo-app/frontend/src/todo/todo.jsx
@@ -8,58 +8,6 @@ import TodoList from './todoList'
 const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { description: '', list: []}
-        this.handleAdd = this.handleAdd.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleRemove = this.handleRemove.bind(this)
-        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
-        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
-        this.handleSearch = this.handleSearch.bind(this)
-        this.handleClear = this.handleClear.bind(this)
-        this.refresh()
-    }
-
-    refresh(description = '') {
-        const search = description ? `&description__regex=/${description}/` : ''
-        axios.get(`${URL}?sort=-createdAt${search}`)
-            .then((resp) => this.setState({ ...this.setState, description, list: resp.data}))
-    }
-
-    handleClear() {
-        this.refresh()
-    }
-
-    handleSearch() {
-        this.refresh(this.state.description)
-    }
-
-    handleMarkAsPending(todo) {
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handleMarkAsDone(todo) {
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh(this.state.description))
-    }
-
-    handleChange(e) {
-        this.setState({ ...this.state, description: e.target.value })
-    }
-
-    handleAdd() {
-        const description = this.state.description
-        axios.post(URL, { description })
-            .then(resp => this.refresh())
-    }
-
     render() {
         return (
             <div>
