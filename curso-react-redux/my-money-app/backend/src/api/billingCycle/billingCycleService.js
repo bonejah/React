9eae36f89fba52af3bf44e2aad0652450a457d42
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 12:21:15 -0200

    add exercises

--- curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true }) // importante para utilizar o node- restful
 
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if(error){
+      res.status(500).json({ errors: [error] })
+    }else{
+      res.json({ value })
+    }
+  })
+})
+
 module.exports = BillingCycle
