3951b52e5dab94eb02f5a81df1c0cc038dcafd6b
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 13:28:43 -0200

    first commit my-money-app

--- curso-react-redux/my-money-app/backend/src/config/server.js
+++ curso-react-redux/my-money-app/backend/src/config/server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
+// Middlewares
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function(){
   console.log(`BACKEND is running on port ${port}.`)
