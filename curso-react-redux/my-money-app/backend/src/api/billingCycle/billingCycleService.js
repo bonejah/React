63036e7c7beadf7c439a658804fd05dec638ddc2
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-11 12:37:51 -0200

    add exercises

--- curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ curso-react-redux/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -13,4 +13,20 @@ BillingCycle.route('count', (req, res, next) => {
   })
 })
 
+BillingCycle.route('summary',  (req, res, next) => {
+  BillingCycle.aggregate({
+    $project: { credits: { $sum: "$credits.value" }, debits: { $sum: "$debits.value" }}
+  }, {
+    $group: { _id: null, credit: { $sum: "$credits" }, debit: { $sum: "$debits" }}
+  }, {
+    $project: { _id: 0, credit: 1, debit: 1 }
+  }, (error, result) => {
+    if(error){
+      res.status(500).json({ errors: [error] })
+    }else{
+      res.json({ result })
+    }
+  })
+})
+
 module.exports = BillingCycle
