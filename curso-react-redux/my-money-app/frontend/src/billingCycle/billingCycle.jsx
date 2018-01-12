9b0307357d3fcd1d707e359d04dd144135a914a8
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-12 16:57:36 -0200

    add exercises

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux' 
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+import tabContent from '../common/tab/tabContent';
+import { selectTab, showTabs } from '../common/tab/tabAction'
 
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList');
+    this.props.showTabs('tabList', 'tabCreate')
+  }
+
   render() {
     return (
       <div>
@@ -20,7 +31,10 @@ class BillingCycle extends Component {
               <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
             </TabsHeader>
             <TabsContent>
-
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Deletar</h1></TabContent>
             </TabsContent>
           </Tabs>  
          </Content> 
@@ -29,4 +43,5 @@ class BillingCycle extends Component {
   }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
