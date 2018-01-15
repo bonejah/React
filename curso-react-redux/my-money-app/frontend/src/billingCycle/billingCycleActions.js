55b674d6906959f7c3c7714628a3d3aee8e2da10
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-15 18:05:47 -0200

    add new exercices

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -19,6 +19,9 @@ export function create(values){
     .then(resp => {
       toastr.success('Sucesso', 'Operação realizada com sucesso')
     })
+    .catch(e => {
+      e.response.data.errors.forEach(error => toastr.error('Erro', error))
+    })
   return {
     type: 'TEMP'
   }
