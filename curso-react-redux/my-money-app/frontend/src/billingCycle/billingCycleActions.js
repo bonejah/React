350a363231bc81027208378dd426fd8c0efa011b
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-16 09:35:07 -0200

    add new exercices

--- curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ curso-react-redux/my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
-import { reset as resetForm } from 'redux-form'
+import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabAction'
 
 const BASE_URL = 'http://localhost:3003/api'
@@ -32,3 +32,12 @@ export function create(values){
       })
   }
  }
+
+ export function showUpdate(billingCycle){
+   return[
+     showTabs('tabUpdate'),
+     selectTab('tabUpdate'),
+     initialize('billingCycleForm', billingCycle)
+   ]
+ }
+
