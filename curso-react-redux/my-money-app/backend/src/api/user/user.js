bc00326b3a86d919e30efebafa77cee619a202a1
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-17 13:56:59 -0200

    add new exercises

--- curso-react-redux/my-money-app/backend/src/api/user/user.js
+++ curso-react-redux/my-money-app/backend/src/api/user/user.js
@@ -1,10 +1,10 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const userSchemma = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, min: 6, max: 12, required: true }
 })
 
-module.exports = restful.model('User', userSchemma)
+module.exports = restful.model('User', userSchema)
