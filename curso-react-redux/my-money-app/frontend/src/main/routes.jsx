df411bd8240e33eb506e3b8a616fa483113ab6ba
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-17 16:50:42 -0200

    add new exercises

--- curso-react-redux/my-money-app/frontend/src/main/routes.jsx
+++ curso-react-redux/my-money-app/frontend/src/main/routes.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import App from './app'
+import AuthOrApp from './authOrApp'
 import DashBoard from '../dashboard/dashboard'
 //import DashBoard from '../dashboard2/dashboard2' // Componente sem REDUX
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
   <Router history={hashHistory}>
-    <Route path='/' component={App}> 
+    <Route path='/' component={AuthOrApp}>}> 
       <IndexRoute component={DashBoard} />
       <Route path='billingCycles' component={BillingCycle} />
     </Route>
