273d59d04c4e87c35b92479f17eb802c82f7920e
Bruno Paulo Costa Lima <bplima@unicid.edu.br>
2018-01-17 14:15:37 -0200

    add new exercises

--- curso-react-redux/my-money-app/backend/src/config/routes.js
+++ curso-react-redux/my-money-app/backend/src/config/routes.js
@@ -1,12 +1,20 @@
-const express = require('express')
+const express = require("express");
 
 module.exports = function(server) {
+  
+  /* Rotas protegidas por Token JWT  */ 
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
+  protectedApi.use(auth); // IMPORTANTE essa linha faz passar pela autenticacao
 
-  // Definir URL base para todas as rotas
-  const router = express.Router()
-  server.use('/api', router)
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
 
-  // Rotas de Ciclo de Pagamento
-  const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
-}
+  /*      * Rotas abertas      */ 
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+  const AuthService = require("../api/user/AuthService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
+};
