d2375e959b2159afa673cbc0507b83114c960023
Bruno Lima <brunopclima81@gmail.com>
2019-10-17 05:35:36 -0300

    add firebase in application

--- react-developer-2019/crwn-clothing/src/components/custom-button/custom-button.component.jsx
+++ react-developer-2019/crwn-clothing/src/components/custom-button/custom-button.component.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './custom-button.styles.scss';
+import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, ...props }) => (
-  <button className="custom-button" {...props}>
+const CustomButton = ({ children, isGoogleSignIn, ...props }) => (
+  <button
+    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    {...props}
+  >
     {children}
   </button>
-)
+);
 
 export default CustomButton;
