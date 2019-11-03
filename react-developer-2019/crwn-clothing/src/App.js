67d1a6991bb2829f0ae506cafcf1a396a45a7d4f
Bruno Lima <brunopclima81@gmail.com>
2019-11-03 08:21:53 -0300

    implements sign-up component

--- react-developer-2019/crwn-clothing/src/App.js
+++ react-developer-2019/crwn-clothing/src/App.js
@@ -3,27 +3,40 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.util';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log('user', user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state.currentUser);
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -41,7 +54,7 @@ class App extends React.Component {
           <Route path="/signin" component={SignInAndSignUp} />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
