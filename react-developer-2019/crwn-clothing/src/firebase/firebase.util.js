96d90e8bb1dfb54808519d4a1ba1d537293f0fa9
Bruno Lima <brunopclima81@gmail.com>
2019-11-13 10:13:22 -0300

    add redux in App, Header components

--- react-developer-2019/crwn-clothing/src/firebase/firebase.util.js
+++ react-developer-2019/crwn-clothing/src/firebase/firebase.util.js
@@ -24,7 +24,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
   
-
     try {
       await userRef.set({
         displayName,
