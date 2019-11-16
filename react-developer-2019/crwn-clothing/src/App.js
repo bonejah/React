b67243ef6f1f301a2bda9970a5bd3ca651032a51
Bruno Lima <brunopclima81@gmail.com>
2019-11-16 07:05:42 -0300

    updating component to use Redirect

--- react-developer-2019/crwn-clothing/src/App.js
+++ react-developer-2019/crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
@@ -10,6 +10,7 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.util";
+import SignIn from "./components/sign-in/sign-in.component";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -43,15 +44,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
