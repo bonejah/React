6ff42cded2e8bcf0c31e2709bb4d4ab8175ccc36
Bruno Lima <brunopclima81@gmail.com>
2019-11-18 08:42:03 -0300

    finish checkout component and page

--- react-developer-2019/crwn-clothing/src/redux/cart/cart.reducer.js
+++ react-developer-2019/crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { cartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import { addItemToCart, removeItemFromCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -25,6 +25,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
           cartItem => cartItem.id !== action.payload.id
         )
       };
+    case cartActionTypes.REMOVE_ITEM: 
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      }  
     default:
       return state;
   }
